990cd9793984124d8cc4bc41c813a6560094ff85
Rigoberto Rascon <RRASCON@Grupoalta.local>
2021-11-12 16:06:48 -0700

    App slpash

--- assets/commonStyles.js
+++ assets/commonStyles.js
@@ -4,6 +4,7 @@ import { Ubuntu_400Regular } from "@expo-google-fonts/ubuntu";
 export const primary = "#5B5B5B";
 module.exports = StyleSheet.create({
   text: { color: "#5B5B5B", fontWeight: "bold", fontSize: 18 },
+  center: { justifyContent: "center", alignItems: "center" },
   primary: { backgroundColor: "#5B5B5B", color: "#5B5B5B" },
   title: {
     fontSize: 25,
@@ -21,5 +22,19 @@ module.exports = StyleSheet.create({
     justifyContent: "center",
     padding: 15,
     marginTop: 10,
+  },
+  holder: {
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    marginVertical: 15,
+    backgroundColor: "#ffff",
+    borderRadius: 20,
+  },
+  fab: {
+    position: "absolute",
+    margin: 26,
+    right: 0,
+    bottom: 0,
+    elevation: 1,
   },
 });
